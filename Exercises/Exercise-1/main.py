a896b16e105effe02cd653c21afe68b08b456348
yellowflash2041 <yellowflash2041@gmail.com>
2024-07-13 02:16:07 -0700

    Complete Exercise 1

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,9 @@
 import requests
+import zipfile
+import io
+
+from pathlib import Path
+from requests.exceptions import HTTPError
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -10,9 +15,23 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+download_directory = Path(__file__).parent / "downloads"
+
+
+def download_and_unzip(uri: str, download_directory: Path):
+    with requests.Session() as s, s.get(uri, stream=True, timeout=60) as response:
+        try:
+            response.raise_for_status()
+            zipfile.ZipFile(io.BytesIO(response.content)).extractall(download_directory)
+        except HTTPError as h:
+            print(f"Invalid request for {uri}")
+            print(h)
+
 
 def main():
     # your code here
+    for uri in download_uris:
+        download_and_unzip(uri, download_directory)
     pass
 
 
